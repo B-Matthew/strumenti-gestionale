6119148c00b9529845610fed0dc0bb80e79db5f6
Matthew Battistini <matthew.battistini@beaverlab.com>
2025-07-21 16:58:45 +0200

    pwa

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,59 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,jpg,jpeg}"],
+      },
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Gestionale Strumenti",
+        short_name: "Strumenti",
+        description: "App per la gestione di strumenti e operatori",
+        theme_color: "#041B39",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait",
+        scope: "/",
+        start_url: "/",
+        icons: [
+        //   {
+        //     src: "pwa-64x64.png",
+        //     sizes: "64x64",
+        //     type: "image/png",
+        //   },
+        //   {
+        //     src: "pwa-192x192.png",
+        //     sizes: "192x192",
+        //     type: "image/png",
+        //   },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+        //   {
+        //     src: "maskable-icon-512x512.png",
+        //     sizes: "512x512",
+        //     type: "image/png",
+        //     purpose: "maskable",
+        //   },
+        ],
+      },
+      devOptions: {
+        enabled: true, // Per testare in sviluppo
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
